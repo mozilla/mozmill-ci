3f395ca30602fac1be4fd00b7087bd138c016974
whimboo <hskupin@mozilla.com>
2011-12-27 01:05:15 +0100

    Update platform map, add some Tier 1 locales, and update handling of major props

--- pulse.py
+++ pulse.py
@@ -1,31 +1,34 @@
 import json
-import os
+import osm
 import sys
 
 import jenkins
 from mozillapulse import consumers
 
 
-PRODUCT = "firefox"
-BRANCHES = ['mozilla-central', 'mozilla-aurora']
-PLATFORMS = ['linux', 'linux64', 'macosx64', 'win32', 'win64']
-LOCALES = ['en-US', 'de']
+# Configuration settings which have to be moved out of the script
+USER = 'mozilla'
+PASS = 'test1234'
 
+PRODUCTS  = ['firefox', 'thunderbird']
+BRANCHES  = ['mozilla-central', 'mozilla-aurora']
+LOCALES   = ['de', 'en-US', 'fr', 'it', 'ja', 'es-ES', 'pl', 'pt-BR', 'ru', 'tr']
+PLATFORMS = ['linux', 'linux64', 'mac', 'win32', 'win64']
+
+
+# Map to translate platform ids from Pulse to Mozmill / Firefox
 PLATFORM_MAP = {'linux': 'linux',
+                'linux-debug': 'linux',
                 'linux64': 'linux64',
+                'linux64-debug': 'linux64',
                 'macosx': 'mac',
+                'macosx-debug': 'mac',
                 'macosx64': 'mac',
+                'macosx64-debug': 'mac',
                 'win32': 'win32',
-                'win64': 'win64'}
-
-
-#EXTRA_PLATFORMS = ['linux-debug', 'linux64-debug',
-#                   'macosx-debug', 'macosx64-debug',
-#                   'win32-debug']
-
-
-
-j = jenkins.Jenkins('http://localhost:8080') #, 'qa-auto', 'mozqa')
+                'win32-debug': 'win32',
+                'win64': 'win64',
+                'win64-debug': 'win64'}
 
 
 def handle_notification(data, message):
@@ -46,16 +49,29 @@ def handle_notification(data, message):
     else:
         props = dict()
 
+    # Retrieve imporant properties
+    product = props.get('product')
+    branch = props.get('branch')
+    buildid = props.get('buildid')
+    props.get('locale', 'en-US')
+    platform = PLATFORM_MAP[props.get('platform')]
+    version = props.get('appVersion')
+
+
     # If the product doesn't match the expected one we are not interested
-    if not props.get('product') == PRODUCT:
+    if not product in PRODUCTS:
         return
 
     # If the branch is not allowed we are not interested
-    if not props.get('branch') in BRANCHES:
+    if not branch in BRANCHES:
         return
 
     # If the platform is not allowed we are not interested
-    if not props.get('platform') in PLATFORMS:
+    if not platform in PLATFORMS:
+        return
+
+    # If the locale is not allowed we are not interested
+    if not locale in LOCALES:
         return
 
     # Only regular daily builds will provide a previous build id
@@ -67,34 +83,39 @@ def handle_notification(data, message):
     if props.has_key('installerFilename'):
         url = '/'.join([os.path.dirname(url), props.get('installerFilename')])
 
-    print "Routing Key: %s - Branch: %s" % (routing_key, props.get('branch'))
+    print "Routing Key: %s - Branch: %s" % (routing_key, branch)
     print "%(PRODUCT)s %(VERSION)s %(PLATFORM)s %(LOCALE)s %(BUILDID)s %(PREV_BUILDID)s" % {
-              'PRODUCT': props.get('product'),
-              'VERSION': props.get('appVersion'),
-              'PLATFORM': props.get('stage_platform'),
-              'LOCALE': props.get('locale', 'en-US'),
-              'BUILDID': props.get('buildid'),
+              'PRODUCT': product,
+              'VERSION': version,
+              'PLATFORM': platform,
+              'LOCALE': locale,
+              'BUILDID': buildid,
               'PREV_BUILDID': props.get('previous_buildid'),
               }
     print "Download: %s" % url
     print "Props: %s\n\n" % json.dumps(props)
 
-    if not 'mac' in props['platform'] or not props['locale'] in LOCALES:
-        return
-
-    j.build_job('update-test', {'BRANCH': props['branch'],
-                                'PLATFORM': PLATFORM_MAP[props['platform']],
-                                'LOCALE': props['locale'],
+    j.build_job('update-test', {'BRANCH': branch,
+                                'PLATFORM': platform,
+                                'LOCALE': locale,
                                 'BUILD_ID': props['previous_buildid'],
-                                'TARGET_BUILD_ID': props['buildid'] })
+                                'TARGET_BUILD_ID': buildid })
 
-pulse = consumers.BuildConsumer(applabel='qa-auto@mozilla.com|daily_testrun')
-pulse.configure(topic='#', callback=handle_notification)
 
-while True:
-    try:
-        pulse.listen()
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except Exception, e:
-        print str(e)
+def main():
+    j = jenkins.Jenkins('http://localhost:8080', USER, PASS)
+
+    pulse = consumers.BuildConsumer(applabel='qa-auto@mozilla.com|daily_testrun')
+    pulse.configure(topic='#', callback=handle_notification)
+
+    while True:
+        try:
+            pulse.listen()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception, e:
+            print str(e)
+
+
+if __name__ == "__main__":
+    main()
