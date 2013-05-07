cfeaa79550701411c5bef7f387f6403d458c3b7a
Henrik Skupin <mail@hskupin.info>
2013-05-07 10:46:06 +0200

    Ensure to start Jenkins always with jenkins-env sourced first (#235)

--- start.sh
+++ start.sh
@@ -10,6 +10,8 @@ export JENKINS_HOME=$(dirname $BASH_SOURCE)/jenkins-master
 echo "Downloading Jenkins $JENKINS_VERSION from $JENKINS_URL"
 curl --location $JENKINS_URL -z $JENKINS_WAR -o $JENKINS_WAR
 
+source jenkins-env/bin/activate
+
 # TODO: Start Jenkins as daemon
 java -jar -Xms2g -Xmx2g -XX:MaxPermSize=512M -Xincgc $JENKINS_WAR
 
