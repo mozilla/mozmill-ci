62e754b40370affe5e0686f9e65ba2697375f9e0
Henrik Skupin <hskupin@mozilla.com>
2016-10-25 15:00:33 +0200

    Add logging to start.py for download output (#819)

--- start.py
+++ start.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -u
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import logging
 import os
 import sys
 
+logging.basicConfig(format='%(levelname)s | %(message)s', level=logging.INFO)
+logger = logging.getLogger('jenkins')
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -20,21 +23,26 @@ JENKINS_WAR = os.path.join(HERE, 'war', 'jenkins-%s.war' % JENKINS_VERSION)
 def start_jenkins():
     try:
         execfile(JENKINS_ENV, dict(__file__=JENKINS_ENV))
-        print "Virtual environment activated successfully."
-    except Exception as ex:
-        print 'Could not activate virtual environment at "%s": %s.' % (JENKINS_ENV, str(ex))
+        logger.info('Virtual environment activated successfully.')
+    except Exception:
+        logger.exception('Could not activate virtual environment at "%s"' % JENKINS_ENV)
         sys.exit(1)
 
     # do imports here because it requires the virtualenv to b activated
-    from mozdownload import DirectScraper
     from mozprocess.processhandler import ProcessHandler
 
-    # Download the Jenkins WAR file
-    scraper = DirectScraper(url=JENKINS_URL, destination=JENKINS_WAR)
-    scraper.download()
+    # Download the Jenkins WAR file if necessary
+    if not os.path.exists(JENKINS_WAR):
+        download_args = ['wget', JENKINS_URL, '-x', '-O', JENKINS_WAR]
+        proc = ProcessHandler(download_args)
+        proc.run()
+        retval = proc.wait()
+
+        if retval != 0:
+            raise Exception('Failure downloading file "%s"' % JENKINS_URL)
 
     # TODO: Start Jenkins as daemon
-    print "Starting Jenkins"
+    logger.info('Starting Jenkins')
     os.environ['JENKINS_HOME'] = os.path.join(HERE, 'jenkins-master')
     args = ['java', '-Xms2g', '-Xmx2g', '-XX:MaxPermSize=512M',
             '-Xincgc', '-jar', JENKINS_WAR]
@@ -43,7 +51,7 @@ def start_jenkins():
     return proc
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     proc = start_jenkins()
 
     try:
